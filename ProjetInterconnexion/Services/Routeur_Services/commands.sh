590fa5d7b6536e0f0e3f27522a71a60f81f562c2
ayasahir <139578130+ayasahir@users.noreply.github.com>
2024-11-18 18:13:05 +0100

    Delete ProjetInterconnexion/Services/Routeur_Services directory

--- ProjetInterconnexion/Services/Routeur_Services/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo su
-#ip route add 120.0.168.0/21 via 120.0.176.X via ethX
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo -e "\033[0;31mBonjour je suis Routeur Services\033[0m"
-tail -f /dev/null
-
