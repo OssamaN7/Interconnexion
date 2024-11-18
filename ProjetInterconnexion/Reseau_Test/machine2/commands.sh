5e5c98fb9e3bcc71b99064da44959691331b0f9d
simox20031010 <simonacer2003@gmail.com>
2024-11-18 18:21:44 +0100

    Update commands.sh

--- ProjetInterconnexion/Reseau_Test/machine2/commands.sh
+++ ProjetInterconnexion/Reseau_Test/machine2/commands.sh
@@ -2,6 +2,5 @@
 sudo su
 ip route add 120.0.160.0/21 via 120.0.168.4
 ip route add 120.0.176.0/21 via 120.0.168.4
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo -e "\033[0;31mBonjour je suis la machine4\033[0m"
+
 tail -f /dev/null
