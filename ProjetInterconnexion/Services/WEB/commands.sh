000fffeca759fe186653c22d29f7e8b37b5f32ec
ayasahir <139578130+ayasahir@users.noreply.github.com>
2024-11-18 18:11:21 +0100

    Add files via upload

--- ProjetInterconnexion/Services/WEB/commands.sh
+++ ProjetInterconnexion/Services/WEB/commands.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+sudo su 
+#Ajouter les routes
+ip route add 120.0.168.0/21 via 120.0.160.4 dev eth0
+ip route add 120.0.176.0/21 via 120.0.160.4 dev eth0
+service apache2 start
+echo -e "\033[0;31mBonjour je suis le serveur WEB\033[0m"
+tail -f /dev/null
