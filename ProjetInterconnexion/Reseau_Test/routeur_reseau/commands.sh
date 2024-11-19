76728c941abd9064a8e9d21293134398d012e558
Ossama AIT-EL MOUDDENE <151570392+OssamaN7@users.noreply.github.com>
2024-11-19 23:30:50 +0100

    Delete ProjetInterconnexion directory

--- ProjetInterconnexion/Reseau_Test/routeur_reseau/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
- echo 1 > /proc/sys/net/ipv4/ip_forward
-
-ip route add 120.0.160.0/21 via 120.0.176.4
-
-tail -f /dev/null
--- ProjetInterconnexion/Services/DNS/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo su 
-ip route add 120.0.168.0/21 via 120.0.160.4 dev eth0
-ip route add 120.0.176.0/21 via 120.0.160.4 dev eth0
-systemctl enable bind9
-echo -e "\033[0;31mBonjour je suis le serveur DNS\033[0m"
-tail -f /dev/null
