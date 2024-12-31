ef3bcf252f5cea69a41c0e1b1c52d26cf9e295d4
ayasahir <139578130+ayasahir@users.noreply.github.com>
2024-12-31 13:06:12 +0100

    Add files via upload

---
+++ Projet_Interconnexion/Reseau_Test/machine2/commands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Fonction pour ajouter une route avec des tentatives répétées
+add_route() {
+  local route="$1"
+  local gateway="$2"
+
+  until ip route add "$route" via "$gateway"; do
+    echo "Tentative d'ajout de la route $route via $gateway..."
+    sleep 1
+  done
+
+  echo "Route $route via $gateway ajoutée avec succès."
+}
+
+# Active le forwarding IP
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
+# Ajoute les routes avec vérification
+add_route "120.0.160.0/21" "192.168.32.40"
+add_route "120.0.176.0/21" "192.168.32.40"
+# Garder le conteneur actif
+
+tail -f /dev/null
