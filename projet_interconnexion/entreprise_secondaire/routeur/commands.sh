aa49c0fddd9572cc2964d6e5d0838f83476e16e5
raid_ghallabi <159778417+raidghal@users.noreply.github.com>
2025-01-14 17:01:08 +0100

    Update commands.sh

--- projet_interconnexion/entreprise_secondaire/routeur/commands.sh
+++ projet_interconnexion/entreprise_secondaire/routeur/commands.sh
@@ -4,7 +4,7 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # Ajouter une route vers le sous-réseau de l'entreprise principale
-ip route add 120.0.50.0/24 via 120.0.35.1
+ip route add 120.0.34.0/24 via 120.0.35.1
 
 # Garder le conteneur actif
 tail -f /dev/null
