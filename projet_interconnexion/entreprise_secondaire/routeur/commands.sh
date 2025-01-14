f5bed2eb8c76cf4a4e8b3be413e7185a00c799c5
raid_ghallabi <159778417+raidghal@users.noreply.github.com>
2025-01-14 17:00:06 +0100

    Create commands.sh

---
+++ projet_interconnexion/entreprise_secondaire/routeur/commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activer le routage IP
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Ajouter une route vers le sous-réseau de l'entreprise principale
+ip route add 120.0.50.0/24 via 120.0.35.1
+
+# Garder le conteneur actif
+tail -f /dev/null
